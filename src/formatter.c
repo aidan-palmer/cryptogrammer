6792303f87f7339898a09b768cb250352f8b4a1d
Aidan Palmer <aidanhpalmer@gmail.com>
2025-06-18 10:20:33 -0600

    update

--- src/formatter.c
+++ src/formatter.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv) {}
+void write_csv(int line, int num_sentences, int num_chars, char* quote, char* author) {
+
+}
+
+int count_sentences(char* block) {
+
+}
+
+int count_chars(char* block) {
+
+}
+
+char* parse_author(char* block) {
+
+}
+
+char* parse_quote(char* block) {
+
+}
+
+void read_txt(char* filename) {
+    // open and read file
+    // sort of a "main"
+    // get each block of quote and author, then parse that with parse_quote and parse_author,
+    // then pass that to write_csv
+}
+
+int main(int argc, char** argv) {
+        
+}
 /* 
  * main is where I will run the formatter
+ * add in condition where if quotes.csv already exists, rename it to quotes.old.csv
  * */
